5c86b0c3cee5cb8698bb4db8bf87a4a093439661
Revanth Singothu <revanthsingothu@gmail.com>
2025-10-04 16:32:07 +0000

    ci: whitelisted origins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,4 +9,9 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  trustedOrigins: [
+    "https://localhost:3000",
+    "https://dev.portalai.in",
+    "https://portalai.in",
+  ],
 });
