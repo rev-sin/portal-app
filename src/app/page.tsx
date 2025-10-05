0bfe2cfa8cd73c61b5e997311b09aebec5e66484
Revanth Singothu <revanthsingothu@gmail.com>
2025-10-05 10:19:49 +0000

    ui: added sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import CTA from "@/components/sections/cta/default";
+import FooterSection from "@/components/sections/footer/default";
+import Hero from "@/components/sections/hero/default";
 
 export default function Home() {
-  const router = useRouter();
-  return (
-    <div className="font-sans justify-items-center">
-      <main className="flex-column justify-items-center">
-        <h1 className="text-xl py-5">Portal AI India.</h1>
-        <div className="flex gap-4">
-          <Button
-            type="button"
-            onClick={() => router.push("/sign-up")}
-            className="bg-white text-black font-medium px-6 py-2 rounded-md hover:bg-gray-200"
-          >
-            Sign Up
-          </Button>
-          <Button
-            type="button"
-            onClick={() => router.push("/sign-in")}
-            className="border border-white text-white font-medium px-6 py-2 rounded-md hover:bg-neutral-800"
-          >
-            Sign In
-          </Button>
-        </div>
-      </main>
-    </div>
-  );
+  return (<>
+  <Hero />
+  <CTA />
+  <FooterSection />
+  </>)
 }
